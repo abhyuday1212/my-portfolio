2860dafa9c71dd6dd837ad9fdc5c16e24f33db4a
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2023-08-10 01:02:18 +0530

    day1 issue resolved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,52 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+/*===============  MENU SHOW ===============*/
+// validate if constant exists
 
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===============  MENU HIDDEN ===============*/
+
+// Validate if constant exists
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+// ==============change BAckground header ========
+// const scrollHeader = () => {
+//   const header = document.getElementById("header");
+//   this.scrollY >= 50
+//     ? header.classList.add("scroll-header")
+//     : header.classList.remove("scroll-header");
+// };
+// window.addEventListener("scroll", "scroll-header");
 
 /*=============== SHADOW HEADER ===============*/
 
-
 /*=============== EMAIL JS ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
