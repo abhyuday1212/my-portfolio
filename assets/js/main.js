f7fb3b5c40db8232b9b4097795e6b29f8aee86ae
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2023-08-10 13:53:23 +0530

    home completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@ if (navToggle) {
 
 // Validate if constant exists
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-  })
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
@@ -26,18 +26,18 @@ const navLink = document.querySelectorAll(".nav__link");
 const linkAction = () => {
   const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show-menu");
-}
+};
 
-navLink.forEach(n => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // ==============change BAckground header ========
-// const scrollHeader = () => {
-//   const header = document.getElementById("header");
-//   this.scrollY >= 50
-//     ? header.classList.add("scroll-header")
-//     : header.classList.remove("scroll-header");
-// };
-// window.addEventListener("scroll", "scroll-header");
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SHADOW HEADER ===============*/
 
