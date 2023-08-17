b647f5d0c48c21e63d5fd7cbb92744ed1ede2189
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2023-08-18 01:31:33 +0530

    till projects addedd

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,34 @@ const shadowHeader = () => {
     : header.classList.remove("shadow-header");
 };
 window.addEventListener("scroll", shadowHeader);
+// ============== Projects
 
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
 
 /*=============== EMAIL JS ===============*/
 
