18cf28025653d5a52a70e90f70d4905c2bdd7f25
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2023-08-20 02:09:50 +0530

    email added

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,7 @@ let swiperCards = new Swiper(".card__content", {
     prevEl: ".swiper-button-prev",
   },
 
-  breakpoints:{
+  breakpoints: {
     600: {
       slidesPerView: 2,
     },
@@ -68,6 +68,40 @@ let swiperCards = new Swiper(".card__content", {
 });
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_g4kt2ys",
+      "template_nuoix3d",
+      "#contact-form",
+      "T7P6vEkT9oSunJBZG"
+    )
+    .then(
+      () => {
+        //show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
 
