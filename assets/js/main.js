896a466b2a185b5bffe679465766c9aa1f8bd364
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2023-08-23 20:11:03 +0530

    animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,22 +88,23 @@ const sendEmail = () => {
       document.getElementById("subject").value +
       "<br> Message:" +
       document.getElementById("message").value,
-  })
-  .then(() => {
-    //show sent message
-    contactMessage.textContent = "Message sent successfully ✅";
+  }).then(
+    () => {
+      //show sent message
+      contactMessage.textContent = "Message sent successfully ✅";
 
-    // Remove message after five seconds
-    setTimeout(() => {
-      contactMessage.textContent = "";
-    }, 5000);
+      // Remove message after five seconds
+      setTimeout(() => {
+        contactMessage.textContent = "";
+      }, 5000);
 
-    // Clear input fields
-    contactForm.reset();
-  }, () => {
-        contactMessage.textContent = "Message NOT sent ❌";
-  }
-  )
+      // Clear input fields
+      contactForm.reset();
+    },
+    () => {
+      contactMessage.textContent = "Message NOT sent ❌";
+    }
+  );
 };
 
 // ================== typed js =============================
@@ -145,24 +146,63 @@ function initializeTyped() {
 }
 initializeTyped();
 
-// window.addEventListener("scroll", updateURL);
-//     function updateURL() {
-//       const scrollY = window.scrollY;
-//       const url = `#scroll-${scrollY}`;
-//       history.replaceState({}, document.title, url);
-// }
-
-// window.addEventListener('scroll', function () {
-//   if (window.location.hash === '#scroll-933.5') {
-//     initializeTyped();
-//   }
-
-// });
-
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  if (this.scrollY >= 350) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+};
+
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true  //animations repeat
+});
+
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'right' })
+sr.reveal(`.home__name,.home__info, .about__container , .section__title-1 , .about__info, .contact__social, .contact__data`,{origin: 'left'})
+sr.reveal(`.services__card, card__container`, { interval: 100 });
+
+
+
+
+
+
+
+
+
+
