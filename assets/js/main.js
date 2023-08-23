fd1fec258d60fc7f3f8d6e2370025a588bbfaeda
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2023-08-23 20:16:45 +0530

    animation updated 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -194,7 +194,10 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'right' })
-sr.reveal(`.home__name,.home__info, .about__container , .section__title-1 , .about__info, .contact__social, .contact__data`,{origin: 'left'})
+sr.reveal(
+  `.home__name,.home__info, .about__container , .section__title-1 , .about__info, .contact__social, .contact__data,.autotype1`,
+  { origin: "left" }
+);
 sr.reveal(`.services__card, card__container`, { interval: 100 });
 
 
